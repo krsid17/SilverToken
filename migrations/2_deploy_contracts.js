8a9a9fb0e31a2ae286dc355ee373c98e0efc9458
Sidhant <sidhant17.sk@gmail.com>
2022-05-18 10:52:04 +0530

    Second Commit

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,5 +2,5 @@ var SilverToken = artifacts.require("SilverToken");
 
 module.exports = function(deployer) {
   // Arguments are: contract, initialSupply
-  deployer.deploy(SilverToken, 1000);
+  deployer.deploy(SilverToken, 10000);
 };
